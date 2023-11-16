2d288ddb0ab39736732eaaf847356b74e591b688
Steven <steven.agbassah@student.vinci.be>
2023-11-16 14:08:05 +0100

    Correction url callback

--- backend/routes/discord.js
+++ backend/routes/discord.js
@@ -6,7 +6,7 @@ const axios = require('axios')
 
 const CLIENT_ID = '1174682089338183772'
 const CLIENT_SECRET = '-q2j9dJchOJ2KcZAhKZpMAy0PAMY22qr'
-const REDIRECT_URI = "http://localhost:3000/discord/api/callback"
+const REDIRECT_URI = "http://localhost:3000/discord/callback"
 
 
 router.get('/login', async function(req, res, next) {
@@ -16,7 +16,7 @@ router.get('/login', async function(req, res, next) {
 
 
 
-router.get("/api/callback", async (request, res) => {
+router.get("/callback", async (request, res) => {
     const code = request.query["code"]
     const resp = await axios.post('https://discord.com/api/oauth2/token',
         new URLSearchParams({
