4c492120f75cc60e3a1581bef37cc97eb1c2f3d5
Brahim <brahim.abid@student.vinci.be>
2023-11-17 09:21:05 +0100

    Init database

---
+++ backend/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  idRiot: String,
+  idGithub: String,
+  idGoogle: String,
+  profilePicture: String,
+  email: String,
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+// Export model
+module.exports = mongoose.model("User", userSchema);
