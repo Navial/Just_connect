fff7e28d11f532bd46d50db5d4d0948072e0935b
Steven <steven.agbassah@student.vinci.be>
2023-11-17 17:52:10 +0100

    Ajouter d'un graphique avec d3.js dans le frontend

---
+++ frontend/src/components/Discord/DiscordStatistics.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import * as d3 from "d3";
+
+const DiscordStatistics = ({ guilds }) => {
+  console.log(guilds);
+  var data = [];
+  guilds.forEach((guild) => {
+    data.push({
+      name: guild.name,
+      featuresNumber: guild.features.length,
+    });
+  });
+
+  const width = 800;
+  const height = 500;
+  const margin = { top: 20, right: 20, bottom: 150, left: 40 };
+
+  const yScale = d3.scaleLinear().domain([0, d3.max(data, (d) => d.featuresNumber)]).range([height, 0]);
+  const xScale = d3.scaleBand().domain(data.map((d) => d.name)).range([0, width]).padding(0.1);
+
+  // Création de l'axe y
+  const yAxis = d3.axisLeft(yScale);
+
+  // Création de l'axe x
+  const xAxis = d3.axisBottom(xScale);
+
+  // Création du conteneur du graphique
+  const svg = d3
+    .select("#discord-chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  
+  svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d) => xScale(d.name))
+    .attr("y", (d) => yScale(d.featuresNumber))
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => height - yScale(d.featuresNumber))
+    .attr("fill", "#7289da");
+
+  
+  svg.append("g").call(yAxis);
+
+  
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(xAxis)
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-45)");
+
+
+  svg
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - height / 2)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Nombre de fonctionnalités");
+
+  return <div id="discord-chart"></div>;
+};
+
+export default DiscordStatistics;
--- frontend/src/components/Discord/UserDiscordInformations.jsx
+++ frontend/src/components/Discord/UserDiscordInformations.jsx
@@ -1,67 +1,80 @@
 import React from 'react';
-import  {useEffect } from "react";
-import axios from 'axios';
 import "./UserDiscordPage.css"
 
+
 import { Container, ListGroup, Row, Col, Tab } from 'react-bootstrap';
+import DiscordStatistics from './DiscordStatistics';
 
 const UserDiscordInformation = ({ user, guilds }) => {
-  
+
+  if(!(user && guilds)) {
+    return (
+      <p>Chargement des informations ... </p>
+    )
+  }
+  else {
 
   return (
-    <Container className="mt-4 text-discord">
-      <Row>
-        <Col>
-          <h2 className="mb-4 title">Informations de l'utilisateur</h2>
-          {user ? (
-            <ul className="list-group user-info-list">
-              <li className="list-group-item ">Nom: {user.username}</li>
-              <li className="list-group-item">Global Name: {user.global_name}</li>
-              <li className="list-group-item">Langue utilisée sur discord : {user.locale}</li>
-              <li className="list-group-item">
-                Avatar: <img src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} alt="Avatar" className="img-fluid" />
-              </li>
-            </ul>
-          ) : (
-            <p>Chargement des informations...</p>
-          )}
-        </Col>
+      <Container className="mt-4 text-discord">
+        <Row>
+          <Col>
+            <h2 className="mb-4 title">Informations de l'utilisateur</h2>
+            {user ? (
+              <ul className="list-group user-info-list">
+                <li className="list-group-item ">Nom: {user.username}</li>
+                <li className="list-group-item">Global Name: {user.global_name}</li>
+                <li className="list-group-item">Langue utilisée sur discord : {user.locale}</li>
+                <li className="list-group-item">
+                  Avatar: <img src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} alt="Avatar" className="img-fluid" />
+                </li>
+              </ul>
+            ) : (
+              <p>Chargement des informations...</p>
+            )}
+          </Col>
 
-        <Tab.Container id="list-group" defaultActiveKey="#link0">
-          <h2 className="mb-4 title">Liste des serveurs </h2>
-          <Row>
-            <Col sm={4} className="server-list">
-              {guilds ? (
-              <ListGroup>
-                {guilds.map((guild, i) => (
-                <ListGroup.Item action href={`#link${i}`} key={guild.id} className="list-group-item"> Name: {guild.name}</ListGroup.Item>
-            ))}
-              </ListGroup>) :  (<p>Chargement des informations...</p>)}
-            </Col>
-            <Col sm={8}>
-              <Tab.Content>
-                {guilds.map((guild, i ) =>(
-                  <Tab.Pane key={guild.id} eventKey={`#link${i}`}>
-                    <p> ID du serveur : {guild.id}</p>
-                    <p> Features du serveur : {guild.features && guild.features.length !== 0 ? guild.features.join(', ') : 'Pas de features disponibles !'}</p>
-                    {guild.icon ? (
-                      <p> Photo du serveur : <img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`} alt="Icon" /></p>
-                    ) : (
-                      <p> Photo du serveur : Pas d'icone disponible pour ce serveur ! </p>
-                    )}
-                    <p> ID du serveur : {guild.id}</p>
+          <Tab.Container id="list-group" defaultActiveKey="#link0">
+            <h2 className="mb-4 title">Liste des serveurs </h2>
+            <Row>
+              <Col sm={4} className="server-list">
+                {guilds ? (
+                <ListGroup>
+                  {guilds.map((guild, i) => (
+                  <ListGroup.Item action href={`#link${i}`} key={guild.id} className="list-group-item"> Name: {guild.name}</ListGroup.Item>
+              ))}
+                </ListGroup>) :  (<p>Chargement des informations...</p>)}
+              </Col>
+              <Col sm={8}>
+                <Tab.Content>
+                  {guilds.map((guild, i ) =>(
+                    <Tab.Pane key={guild.id} eventKey={`#link${i}`}>
+                      <p> ID du serveur : {guild.id}</p>
+                      <p> Features du serveur : {guild.features && guild.features.length !== 0 ? guild.features.join(', ') : 'Pas de features disponibles !'}</p>
+                      {guild.icon ? (
+                        <p> Photo du serveur : <img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`} alt="Icon" /></p>
+                      ) : (
+                        <p> Photo du serveur : Pas d'icone disponible pour ce serveur ! </p>
+                      )}
+                      <p> ID du serveur : {guild.id}</p>
 
 
-                    <p> En suis-je le propriétaire ? :   {guild.owner ?  "Oui" : "Non"}  </p>
-                  </Tab.Pane>
-                ))}
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-      </Row>
-    </Container>
-  );
+                      <p> En suis-je le propriétaire ? :   {guild.owner ?  "Oui" : "Non"}  </p>
+
+                      
+                    </Tab.Pane>
+                  ))}
+                  
+                </Tab.Content>
+                <h2 className = 'title'> Statistiques sur les fonctionnalités des serveurs </h2>
+                <div>{guilds ?  <DiscordStatistics guilds = {guilds}></DiscordStatistics>   :  "Chargement des informations ..."}</div>
+              </Col>
+            </Row>
+            
+          </Tab.Container>
+        </Row>
+      </Container>
+    );
+                      }
 };
 
 export default UserDiscordInformation;
