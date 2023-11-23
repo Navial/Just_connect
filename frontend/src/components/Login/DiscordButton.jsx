2067d02dc840684408e5d975ccc0ffcff1cf98b4
Steven <steven.agbassah@student.vinci.be>
2023-11-23 14:11:42 +0100

    Extraction du bouton discord dans son propre composant

---
+++ frontend/src/components/Login/DiscordButton.jsx
@@ -0,0 +1,44 @@
+
+import { Button } from "antd";
+import axios from 'axios'
+import {useContext} from "react";
+import { Context as UserContext  } from "../../contexts/UserContext"; 
+
+const DiscordButton = () =>  {
+
+    const { logged, connect } = useContext(UserContext);
+
+    const handleDiscordLogin = async () => {
+        try {
+          const response = await axios.post('http://localhost:3000/discord/login');
+    
+          connect("discord");
+          window.location.href = response.data.redirectUrl;
+        } catch (error) {
+          console.error('Erreur lors de la connexion :', error);
+        }
+      }
+
+
+      return (
+        <Button
+        type="default"
+        style={{
+          backgroundColor: "#7289da",
+          color: "#ffffff",
+          border: "none",
+          borderRadius: "4px",
+          fontSize: "16px",
+          cursor: "pointer",
+          textAlign: "center"
+        }}
+        onClick={handleDiscordLogin}
+      >
+        Connect with Discord
+      </Button>
+      )
+
+}
+
+
+export default DiscordButton;
